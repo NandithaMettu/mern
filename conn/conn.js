758333651b7d7d61ee52b8963270d178ce3448fc
NandithaMettu <132463471+NandithaMettu@users.noreply.github.com>
2024-05-10 17:31:51 +0530

    Update conn.js

--- conn/conn.js
+++ conn/conn.js
@@ -1,12 +1,8 @@
 
-require('dotenv').config();
 const mongoose = require("mongoose");
 const conn = async () => {
   try {
-    await mongoose.connect(MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    await mongoose.connect(MONGODB_URI);
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection error:", error);
